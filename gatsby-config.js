5918bb7dda5b11587bf76c0c239adb447c2823fc
Ananya Negi <ananya4negi@gmail.com>
2020-09-21 09:15:15 +0530

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,41 @@
 module.exports = {
-  siteMetadata: {
-    title: `General Products Ltd`,
-    description: `Software, services, technical training and initiatives for innovation in the book publishing industry`,
-    author: `@consonance_app`,
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-jss'
+    siteMetadata: {
+        title: `General Products Ltd`,
+        description: `Software, services, technical training and initiatives for innovation in the book publishing industry`,
+        author: `@consonance_app`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `components`,
-        path: `${__dirname}/src/components`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
+    plugins: [{
+            resolve: 'gatsby-plugin-jss'
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `components`,
+                path: `${__dirname}/src/components`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: ``, // This path is relative to the root of the site.
+            },
+        },
+    ],
 }
